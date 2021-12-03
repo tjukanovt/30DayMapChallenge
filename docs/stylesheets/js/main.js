66b2eae478c10073cd687efef7f732a4952b5445
Topi Tjukanov <topi@gispo.fi>
2021-12-03 11:16:41 +0200

    Update navigation

--- docs/stylesheets/js/main.js
+++ docs/stylesheets/js/main.js
@@ -69,4 +69,27 @@ $(function () {
 
 
 
+
+    //===== Back to top
+
+    // Show or hide the sticky footer button
+    $(window).on('scroll', function (event) {
+        if ($(this).scrollTop() > 600) {
+            $('.back-to-top').fadeIn(200)
+        } else {
+            $('.back-to-top').fadeOut(200)
+        }
+    });
+
+
+    //Animate the scroll to yop
+    $('.back-to-top').on('click', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: 0,
+        }, 1500);
+    });
+
+
 });
